23ededc6f8d81d11ce3bb538d9e456fbe35a86ad
iosliutingxin <fmmmf3vq>
2017-09-04 00:19:28 +0800

    初始化UITableView

--- Powr/Powr/ViewController.m
+++ Powr/Powr/ViewController.m
@@ -16,8 +16,75 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self initUITableView];
 }
+
+
+-(void)initUITableView{
+    
+    UITableView *table =[[UITableView alloc]initWithFrame:CGRectMake(0, 110, self.view.frame.size.width, self.view.frame.size.height)];
+    table.delegate  =self;
+    table.dataSource=self;
+    table.backgroundColor = [UIColor  blackColor];
+    [self.view addSubview:table];
+    
+
+
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    
+    static NSString *identifier = @"idntifier";
+    UITableViewCell *cell =[tableView dequeueReusableCellWithIdentifier:identifier];
+    if (!cell)
+    {
+        cell =[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+    
+    cell.textLabel.text=@"hello world";
+    
+
+    
+    return cell;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+
+    return 2;
+}
+
+#pragma mark - UITabelViewDataSource
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+
+    return 40;
+
+}
+
+
+
+
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+//    if (indexPath.row!=0) {
+//        
+//        NSString *row=[NSString stringWithFormat:@"%ld",(long)indexPath.row];
+//        [self performSegueWithIdentifier:@"productSearch" sender:row];
+//        
+//    }
+    
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 1;
+    
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
