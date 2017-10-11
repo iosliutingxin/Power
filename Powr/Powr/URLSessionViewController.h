163ba9ccf5daf2a8f12cc060a0a8ad0244235d01
iosliutingxin <fmmmf3vq>
2017-10-11 23:18:36 +0800

    session

---
+++ Powr/Powr/URLSessionViewController.h
@@ -0,0 +1,13 @@
+//
+//  URLSessionViewController.h
+//  Powr
+//
+//  Created by 北京时代华擎 on 2017/10/11.
+//  Copyright © 2017年 iOS _Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface URLSessionViewController : UIViewController
+
+@end
---
+++ Powr/Powr/URLSessionViewController.m
@@ -0,0 +1,37 @@
+//
+//  URLSessionViewController.m
+//  Powr
+//
+//  Created by 北京时代华擎 on 2017/10/11.
+//  Copyright © 2017年 iOS _Liu. All rights reserved.
+//
+
+#import "URLSessionViewController.h"
+
+@interface URLSessionViewController ()
+
+@end
+
+@implementation URLSessionViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
