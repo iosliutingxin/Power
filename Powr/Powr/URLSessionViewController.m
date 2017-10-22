2354d2247dad114c6eeaea9b044c9652c0f55451
iosliutingxin <fmmmf3vq>
2017-10-22 23:09:48 +0800

    初始化下载页面（开始，暂停，继续）

--- Powr/Powr/URLSessionViewController.m
+++ Powr/Powr/URLSessionViewController.m
@@ -30,6 +30,58 @@
 - (void)viewDidLoad {
     self.view.backgroundColor = [UIColor whiteColor];
     [super viewDidLoad];
+    [self createView];
+
+}
+
+- (void)createView{
+    
+    UIButton *btn1=[UIButton buttonWithType:UIButtonTypeCustom];
+    btn1.frame=CGRectMake(self.view.frame.size.width/2-50, 100, 100, 40);
+    btn1.backgroundColor=[UIColor redColor];
+    btn1.tag =10;
+    [btn1 setTitle:@"开始" forState:UIControlStateNormal];
+    [btn1 addTarget:self action:@selector(startValue:) forControlEvents:UIControlEventTouchDown];
+    [self.view addSubview:btn1];
+    
+    UIButton *stop=[UIButton buttonWithType:UIButtonTypeCustom];
+    stop.frame=CGRectMake(self.view.frame.size.width/2-50, 150, 100, 40);
+    stop.backgroundColor=[UIColor redColor];
+    stop.tag =10;
+    [stop setTitle:@"暂停" forState:UIControlStateNormal];
+    [stop addTarget:self action:@selector(StopValue:) forControlEvents:UIControlEventTouchDown];
+    [self.view addSubview:stop];
+    
+    
+    UIButton *continu=[UIButton buttonWithType:UIButtonTypeCustom];
+    continu.frame=CGRectMake(self.view.frame.size.width/2-50, 200, 100, 40);
+    continu.backgroundColor=[UIColor redColor];
+    continu.tag =10;
+    [continu setTitle:@"继续" forState:UIControlStateNormal];
+    [continu addTarget:self action:@selector(continueValue:) forControlEvents:UIControlEventTouchDown];
+    [self.view addSubview:continu];
+    
+    
+
+}
+
+// 开始
+-(void)startValue:(UIButton *)object{
+    
+    NSLog(@"start");
+}
+
+//暂停
+-(void)StopValue:(UIButton *)object{
+
+    NSLog(@"stop");
+
+}
+
+//继续
+-(void)continueValue:(UIButton *)object{
+
+    NSLog(@"continue");
 
 }
 
